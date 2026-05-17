39f153feb970e54088950836f4612a3141450218
rxhmn1 <abdulrahman.mgitcse@gmail.com>
2026-05-17 13:15:44 +0530

    Completed Django portfolio website

--- TASK1/main/models.py
+++ TASK1/main/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+
+    title = models.CharField(max_length=100)
+
+    description = models.TextField()
+
+    tech = models.CharField(max_length=200)
+
+    github = models.URLField()
+
+    def __str__(self):
+
+        return self.title
+
+class Message(models.Model):
+
+    name = models.CharField(max_length=100)
+
+    email = models.EmailField()
+
+    message = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+
+        return self.name
--- TASK1/main/views.py
+++ TASK1/main/views.py
@@ -1,5 +1,31 @@
 from django.shortcuts import render
 
+from .models import Project, Message
+
 def home(request):
 
-    return render(request, 'main/home.html')
+    if request.method == 'POST':
+
+        name = request.POST.get('name')
+
+        email = request.POST.get('email')
+
+        message = request.POST.get('message')
+
+        Message.objects.create(
+
+            name=name,
+            email=email,
+            message=message
+
+        )
+
+    projects = Project.objects.all()
+
+    context = {
+
+        'projects': projects
+
+    }
+
+    return render(request, 'main/home.html', context)
